c052dc89280d109c8c5f5b2eb15149864c5a3eeb
huynh <huynhtee33@gmail.com>
2020-09-21 13:05:10 +0700

    simple header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  width: 100%;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
+  margin-bottom: 20px;
+`
 
 const HeaderInner = styled.div`
-  padding: 80px 40px;
+  max-width: 1192px;
+  padding: 20px 0;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
 `
-const Nav = styled.nav`
-  margin-top: auto;
-  margin-bottom: auto;
-  z-index: 100;
-  width: 100%;
-  text-align: center;
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
+
+const Logo = styled.div`
+  font-size: 33px;
+  font-family: 'Bebas Neue', sans-serif;
 `
 
 const Header = () => {
-  return <Wrapper />
+  return (
+    <Wrapper>
+      <HeaderInner>
+        <Logo>
+          <a href="/">Huynh Dev</a>
+        </Logo>
+      </HeaderInner>
+    </Wrapper>
+  )
 }
 
 export default Header
